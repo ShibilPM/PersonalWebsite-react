3f61db1eda9f90aaf6b8e22bf8e021e566bc92ff
ShibilPM <shibilpm3232@gmail.com>
2021-02-03 11:09:28 +0530

    add about page

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
+import { Route, Switch, Redirect } from "react-router-dom";
+
 import "./App.css";
 import HomePage from "./pages/homepage/homepage.component";
 import Header from "./components/header/header.component";
+import AboutPage from "./pages/aboutPage/aboutPage.component";
 
 function App() {
   return (
     <div className="main">
       <Header />
-      <HomePage />
+      <Switch>
+        <Route path="/" exact component={HomePage} />
+        <Route path="/about" exact component={AboutPage} />
+      </Switch>
     </div>
   );
 }
